81d5e2e107f51d46e15eb97d4d63b98b6fcfcd95
Michele <53474707+mifrugo@users.noreply.github.com>
2021-04-23 11:59:05 +0200

    fixed db schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-db
--- Generation Time: Apr 23, 2021 at 09:27 AM
+-- Generation Time: Apr 23, 2021 at 09:24 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.16
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `powercod`
+-- Database: `powercoders-demo`
 --
 
 -- --------------------------------------------------------
@@ -41,6 +41,41 @@ INSERT INTO `passwords` (`whale_id`, `password`) VALUES
 (6, 'wohowho'),
 (12, 'ahahwuy');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `whales`
+--
+
+CREATE TABLE `whales` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `whales`
+--
+
+INSERT INTO `whales` (`id`, `name`) VALUES
+(6, 'Balaena Mysticetus'),
+(12, 'Balaenoptera Acutorostrata'),
+(13, 'Balaenoptera Bonaerensis'),
+(4, 'Balaenoptera Borealis'),
+(10, 'Balaenoptera Edeni'),
+(2, 'Balaenoptera Musculus'),
+(11, 'Balaenoptera Omurai\r\n'),
+(3, 'Balaenoptera Physalus'),
+(15, 'Delphinapterus Leucas'),
+(8, 'Eubalaena Australis'),
+(7, 'Eubalaena Glacialis'),
+(9, 'Eubalaena Japonica'),
+(17, 'Grampus Griseus'),
+(18, 'Kogia Breviceps'),
+(5, 'Megaptera Novaeangliae'),
+(1, 'Orcinus Orca'),
+(14, 'Physeter Macrocephalus'),
+(16, 'Tursiops Truncatus');
+
 --
 -- Indexes for dumped tables
 --
@@ -50,6 +85,23 @@ INSERT INTO `passwords` (`whale_id`, `password`) VALUES
 --
 ALTER TABLE `passwords`
   ADD UNIQUE KEY `whale` (`whale_id`);
+
+--
+-- Indexes for table `whales`
+--
+ALTER TABLE `whales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `whales`
+--
+ALTER TABLE `whales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
